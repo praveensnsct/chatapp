64e1e7e25f9996eb037159926e83495d299bca26
Praveen <Praveen.Thirumurugan@in.tesco.com>
2016-04-05 11:43:12 +0100

    final commit

---
+++ server.js
@@ -0,0 +1,106 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('public/index.html');
+});
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+var nodeBlog = function () {
+
+    //  Scope.
+    var self = this;
+
+
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
+
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function () {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        }
+    };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function (sig) {
+        if (typeof sig === "string") {
+            console.log('%s: Received %s - terminating sample app ...',
+                new Date(Date.now()), sig);
+            process.exit(1);
+        }
+        console.log('%s: Node server stopped.', new Date(Date.now()));
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function () {
+        //  Process on exit and signals.
+        process.on('exit', function () {
+            self.terminator();
+        });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+            'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function (element, index, array) {
+            process.on(element, function () {
+                self.terminator(element);
+            });
+        });
+    };
+
+
+  
+    self.initialize = function () {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function () {
+        //  Start the app on the specific interface (and port).
+        http.listen(self.port, self.ipaddress, function () {
+            console.log('%s: Node server started on %s:%d ...',
+                Date(Date.now()), self.ipaddress, self.port);
+        });
+    };
+
+
+};
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new nodeBlog();
+zapp.initialize();
+zapp.start();
